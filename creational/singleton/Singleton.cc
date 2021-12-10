5149622120e5341bde739329f6dc240a0b370121
wangmaolin <17601606132@163.com>
2021-12-10 13:57:16 +0800

    feat: thread-safe singletion

--- creational/singleton/Singleton.cc
+++ creational/singleton/Singleton.cc
@@ -1,48 +1,35 @@
-// C++ Design Patterns: Singleton
+// C++ Design Patterns: Thread-safe Singleton
 // 
 // Author: wangmaolin
+#include <pthread.h>
+#include <stdlib.h>
 
-#include <iostream>
-
-class Singleton
+class Singleton 
 {
-public:
+ public:
   Singleton(const Singleton&) = delete;
   Singleton& operator=(const Singleton&) = delete;
 
-  static Singleton* get()
+  static Singleton& instance()
   {
-    if (!instance_)
-    {
-      instance_ = new Singleton();
-    }
-    return instance_;
-  }
-
-  static void reset()
-  {
-    if (instance_) 
-    {
-      delete instance_;
-    }
-  }
-
-  void print()
-  {
-    std::cout << "singleton demo" << std::endl;
+    pthread_once(&ponce_, &Singleton::init);
+    return *instance_;
   }
 
 private:
   Singleton() = default;
+
+  static void destory() { delete instance_; }
+
+  static void init()
+  {
+    instance_ = new Singleton();
+    ::atexit(destory);
+  }
+  
+  static pthread_once_t ponce_;
   static Singleton *instance_;
 };
 
+pthread_once_t Singleton::ponce_ = PTHREAD_ONCE_INIT;
 Singleton* Singleton::instance_ = nullptr;
-
-int main()
-{
-  Singleton::get()->print();
-  Singleton::reset();
-
-  return 0;
-}
