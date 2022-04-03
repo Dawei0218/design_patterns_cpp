e2d50a9ce4e37fa8bcc30f7a71636a89df399e2c
wangmaolin <17601606132@163.com>
2022-04-03 14:54:15 +0800

    feat: Factory

---
+++ creational/factory/Factory.cc
@@ -0,0 +1,40 @@
+// C++ Design Patterns: Factory Method
+// 
+// Author: wangmaolin
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// parse基类
+class IParser
+{
+public:
+  virtual void parse() = 0;
+  virtual ~IParser() = default;
+};
+
+class JosnParser : public IParser
+{
+  virtual void parse() override
+  {
+    cout << "This is JsonParser" << endl;
+  }
+};
+
+// 工厂基类
+class IParseFactory
+{
+public:
+  virtual IParser* createParser() = 0;
+  virtual ~IParseFactory() = default;
+};
+
+class JosnParserFactory : public IParseFactory
+{
+  virtual IParser* createParser() override
+  {
+    return new JosnParser();
+  }
+};
+
