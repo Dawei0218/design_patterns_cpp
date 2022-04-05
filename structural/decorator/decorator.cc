975e3bd89e94e60ec634843b97aede91823d6940
wangmaolin <17601606132@163.com>
2022-04-05 11:28:35 +0800

    feat: decorator

--- structural/decorator/decorator.cc
+++ structural/decorator/decorator.cc
@@ -1,35 +1,52 @@
 // C++ Design Patterns: Decorator Design pattern
-// 
+//
 // Author: wangmaolin
 
+// 基类
 class IStream
 {
 public:
-virtual void read() = 0;
-virtual ~IStream() = default;
+  virtual void read() = 0;
+
+  virtual ~IStream() = default;
 };
 
-class FileInputStream : public IStream
+// FileStream
+class FileStream : public IStream
 {
 public:
   virtual void read() override
   {
-    // read some data from file
   }
 };
 
-// 通过组合方式增强功能，避免了类继承过于复杂
-// 比如还可以增加加密数据的类
-class BufferStream : public IStream
+// MemoryStream
+class MemoryStream : public IStream
 {
-private:
-  IStream* stream_;
 public:
-  BufferStream(IStream* stream) : stream_(stream) {}
+  virtual void read() override
+  {
+  }
+};
+
+// Decorator
+class Decorator : public IStream
+{
+protected:
+  IStream *stream;
+
+  Decorator(IStream *stm) : stream(stm) {}
+};
+
+// BufferStream
+class BufferStream : public Decorator
+{
+public:
+  BufferStream(IStream *stream) : Decorator(stream) {}
 
   virtual void read() override
   {
     // buffer
-    // read some data from file
+    stream->read();
   }
 };
