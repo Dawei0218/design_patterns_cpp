ec2e7665b318bd2e605c3d1f51f677a1c2bbea67
wangmaolin <17601606132@163.com>
2022-04-05 11:53:40 +0800

    feat: strategy

---
+++ behavioral/strategy/Strategy.cc
@@ -0,0 +1,47 @@
+// C++ Design Patterns: Decorator Design pattern
+//
+// Author: wangmaolin
+
+// 根据上下文提供 + - 操作类
+
+class IStrategy
+{
+public:
+  virtual int doOperation(int num1, int num2) = 0;
+
+  virtual ~IStrategy() = default;
+};
+
+// 加法
+class Addition : public IStrategy
+{
+public:
+  virtual int doOperation(int num1, int num2) override
+  { 
+    return (num1 + num2);
+  }
+};
+
+// 减法
+class Subtraction : public IStrategy
+{
+public:
+  virtual int doOperation(int num1, int num2) override
+  { 
+    return (num1 - num2);
+  }
+};
+
+// context
+class Context
+{
+private:
+  IStrategy *strategy;
+public:
+  Context(IStrategy *s) : strategy(s) {}
+
+  void execute(int num1, int num2)
+  {
+    strategy->doOperation(num1, num2);
+  }
+};
