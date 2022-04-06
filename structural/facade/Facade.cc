48f2e6ed68585ed92a2e20e805a8fcc3c5ed0ff0
wangmaolin <17601606132@163.com>
2022-04-06 21:28:12 +0800

    feat: facade pattern

---
+++ structural/facade/Facade.cc
@@ -0,0 +1,25 @@
+// C++ Design Patterns: Facade Design pattern
+//
+// Author: wangmaolin
+
+class A
+{
+public:
+  void p() { /* ...  */ }
+};
+
+class B
+{
+public:
+  void p() { /* ...  */ }
+};
+
+class Facade
+{
+public:
+  void p() 
+  {
+    // A p()
+    // B p()
+  }
+};
